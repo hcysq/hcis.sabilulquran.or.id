71424b225baef2542ed68729c950e4f083975192
hcysq <humancapital.ysq@gmail.com>
2025-10-05 16:51:54 +0700

    Added docker-compose.yml

--- wp-content/themes/fitness-elementor/template-parts/pagination.php
+++
@@ -1,8 +0,0 @@
-<?php if(get_theme_mod('fitness_elementor_show_pagination', true )== true): ?>
-	<?php
-		the_posts_pagination( array(
-			'prev_text' => esc_html__( 'Previous page', 'fitness-elementor' ),
-			'next_text' => esc_html__( 'Next page', 'fitness-elementor' ),
-		) );
-	?>		
-<?php endif; ?>
